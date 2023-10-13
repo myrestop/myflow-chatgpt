54339d1ab1ba4f2d37f4bb8ee0908a0ca59042ae
easepan <easepan@qq.com>
2023-10-13 21:24:31 +0800

    feat: support set spark app key

--- src/main/kotlin/top/myrest/myflow/ai/AssistantSettingsContent.kt
+++ src/main/kotlin/top/myrest/myflow/ai/AssistantSettingsContent.kt
@@ -1,20 +1,28 @@
 package top.myrest.myflow.ai
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.onClick
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.unfbx.chatgpt.entity.chat.ChatCompletion
 import top.myrest.myflow.AppInfo
 import top.myrest.myflow.component.FuncPageScope
+import top.myrest.myflow.component.MyHoverable
+import top.myrest.myflow.component.SettingCheckBox
 import top.myrest.myflow.component.SettingCombo
 import top.myrest.myflow.component.SettingInputText
 import top.myrest.myflow.component.SettingItemRow
@@ -24,63 +32,138 @@ import top.myrest.myflow.component.SettingsContent
 import top.myrest.myflow.language.LanguageBundle
 
 internal class AssistantSettingsContent : SettingsContent {
+
+    @OptIn(ExperimentalFoundationApi::class)
     override val settingsContent: @Composable FuncPageScope.(pluginId: String) -> Unit
         get() = {
             Column(
                 modifier = Modifier.width(400.dp).background(MaterialTheme.colors.secondary).padding(horizontal = 20.dp, vertical = 16.dp),
             ) {
-//                val subscription = ChatGptStreamResults.client.subscription()
-//                SettingItemRow(horizontalArrangement = Arrangement.SpaceBetween) {
-//                    SettingLabelText(AppInfo.currLanguageBundle.shared.account)
-//                    SettingConfigText(subscription.accountName)
-//                }
-//                SettingItemRow(horizontalArrangement = Arrangement.SpaceBetween) {
-//                    SettingLabelText(AppInfo.currLanguageBundle.shared.email)
-//                    SettingConfigText(subscription.billingEmail.toString())
-//                }
-//                SettingItemRow(horizontalArrangement = Arrangement.SpaceBetween) {
-//                    SettingLabelText(LanguageBundle.getBy(Constants.PLUGIN_ID, "balance"))
-//                    SettingConfigText("$" + subscription.hardLimitUsd)
-//                }
-                SettingItemRow {
-                    SettingInputText(
-                        label = "API Key",
-                        labelWidth = 120,
-                        value = Constants.openaiApiKey,
-                        placeholder = LanguageBundle.getBy(Constants.PLUGIN_ID, "input-openai-api-key"),
-                        update = {
-                            AppInfo.runtimeProps.paramMap[Constants.OPEN_API_KEY] = it
-                        },
-                    )
+                var provider by remember { mutableStateOf(Constants.provider) }
+                var currentIsDefault by remember { mutableStateOf(false) }
+                currentIsDefault = provider == Constants.provider
+
+                Row {
+                    listOf(
+                        Constants.OPENAI_PROVIDER to "OpenAI",
+                        Constants.SPARK_PROVIDER to LanguageBundle.getBy(Constants.PLUGIN_ID, "spark-desk"),
+                    ).forEach { pair ->
+                        var modifier = Modifier.height(30.dp)
+                        if (provider == pair.first) {
+                            modifier = modifier.background(MaterialTheme.colors.secondaryVariant)
+                        }
+                        MyHoverable(
+                            contentAlignment = Alignment.Center,
+                            modifier = modifier.onClick { provider = pair.first },
+                            hoveredBackground = MaterialTheme.colors.secondaryVariant,
+                        ) {
+                            Text(
+                                text = pair.second,
+                                fontSize = MaterialTheme.typography.subtitle1.fontSize,
+                                color = MaterialTheme.colors.onSecondary,
+                                modifier = Modifier.padding(end = 12.dp),
+                            )
+                        }
+                    }
                 }
-
-                var model by remember { mutableStateOf(Constants.openaiModel) }
-                SettingCombo(
-                    label = "ChatGPT" + AppInfo.currLanguageBundle.wordSep + LanguageBundle.getBy(Constants.PLUGIN_ID, "chatgpt-model"),
-                    labelWidth = 120,
-                    value = model,
-                    menus = ChatCompletion.Model.values().map { it.getName() },
-                    valueMapper = { it },
-                    onMenuClick = {
-                        model = it
-                        AppInfo.runtimeProps.paramMap[Constants.OPEN_MODEL_KEY] = it
-                    },
-                )
-
-                var temperature: Float by remember { mutableStateOf(Constants.openaiTemperature) }
-                SettingSlider(
-                    label = LanguageBundle.getBy(Constants.PLUGIN_ID, "chatgpt-temperature"),
-                    labelWidth = 120,
-                    value = temperature,
-                    valueRange = 0f..2f,
-                    preContent = {
-                        SettingLabelText("%.2f".format(temperature), null)
-                    },
+                SettingCheckBox(
+                    label = LanguageBundle.getBy(Constants.PLUGIN_ID, "set-as-default"),
+                    checked = currentIsDefault,
                     update = {
-                        temperature = it
-                        AppInfo.runtimeProps.paramMap[Constants.OPEN_TEMPERATURE_KEY] = it
+                        currentIsDefault = it
+                        if (it) {
+                            AppInfo.runtimeProps.paramMap[Constants.PROVIDER_KEY] = provider
+                        } else {
+                            AppInfo.runtimeProps.paramMap.remove(Constants.PROVIDER_KEY)
+                        }
                     },
                 )
+                when (provider) {
+                    Constants.OPENAI_PROVIDER -> OenAiSettings()
+                    Constants.SPARK_PROVIDER -> SparkSettings()
+                }
             }
         }
+
+    @Composable
+    @Suppress("FunctionName")
+    private fun SparkSettings() {
+        SettingItemRow {
+            SettingInputText(
+                label = "App ID",
+                labelWidth = 80,
+                value = Constants.sparkAppId,
+                placeholder = "app id",
+                update = {
+                    AppInfo.runtimeProps.paramMap[Constants.SPARK_APP_ID_KEY] = it
+                },
+            )
+        }
+        SettingItemRow {
+            SettingInputText(
+                label = "API Secret",
+                labelWidth = 80,
+                value = Constants.sparkApiSecret,
+                placeholder = "app secret",
+                update = {
+                    AppInfo.runtimeProps.paramMap[Constants.SPARK_API_SECRET_KEY] = it
+                },
+            )
+        }
+        SettingItemRow {
+            SettingInputText(
+                label = "API Key",
+                labelWidth = 80,
+                value = Constants.sparkApiKey,
+                placeholder = "app id",
+                update = {
+                    AppInfo.runtimeProps.paramMap[Constants.SPARK_API_KEY] = it
+                },
+            )
+        }
+    }
+
+    @Composable
+    @Suppress("FunctionName")
+    private fun OenAiSettings() {
+        SettingItemRow {
+            SettingInputText(
+                label = "API Key",
+                labelWidth = 120,
+                value = Constants.openaiApiKey,
+                placeholder = LanguageBundle.getBy(Constants.PLUGIN_ID, "input-openai-api-key"),
+                update = {
+                    AppInfo.runtimeProps.paramMap[Constants.OPEN_API_KEY] = it
+                },
+            )
+        }
+
+        var model by remember { mutableStateOf(Constants.openaiModel) }
+        SettingCombo(
+            label = "ChatGPT" + AppInfo.currLanguageBundle.wordSep + LanguageBundle.getBy(Constants.PLUGIN_ID, "chatgpt-model"),
+            labelWidth = 120,
+            value = model,
+            menus = ChatCompletion.Model.values().map { it.getName() },
+            valueMapper = { it },
+            onMenuClick = {
+                model = it
+                AppInfo.runtimeProps.paramMap[Constants.OPEN_MODEL_KEY] = it
+            },
+        )
+
+        var temperature: Float by remember { mutableStateOf(Constants.openaiTemperature) }
+        SettingSlider(
+            label = LanguageBundle.getBy(Constants.PLUGIN_ID, "chatgpt-temperature"),
+            labelWidth = 120,
+            value = temperature,
+            valueRange = 0f..2f,
+            preContent = {
+                SettingLabelText("%.2f".format(temperature), null)
+            },
+            update = {
+                temperature = it
+                AppInfo.runtimeProps.paramMap[Constants.OPEN_TEMPERATURE_KEY] = it
+            },
+        )
+    }
 }
